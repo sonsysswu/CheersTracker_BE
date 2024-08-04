3079f0c1c97f6271490c8d1727cd207388f653ed
Lee <i876838@gmail.com>
2024-08-05 03:02:17 +0900

    fix : 커뮤니티 게시글 작성자 직렬화 변경

--- community/serializers.py
+++ community/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Post, Comment
+from user.serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True) 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def create(self, validated_data):
+        # 현재 사용자 정보를 가져와서 author 필드에 추가
+        request = self.context.get('request')
+        validated_data['author'] = request.user
+        return super().create(validated_data)
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
