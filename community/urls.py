2c2f2133951b1be127f32ea9708b9662cd740032
Lee <i876838@gmail.com>
2024-08-04 16:32:26 +0900

    fix : 사용자 커뮤니티 최종

--- community/urls.py
+++ community/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView, CommentDetailView, CommentListCreateView
+from .views import PostListCreateView, PostDetailView, CommentDetailView, CommentListCreateView, UserCommentsView, UserLikedPostsView, UserPostsView
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail')
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+    path('user/posts/', UserPostsView.as_view(), name='user-posts'),
+    path('user/comments/', UserCommentsView.as_view(), name='user-comments'),
+    path('user/liked-posts/', UserLikedPostsView.as_view(), name='user-liked-posts'),
 ]
