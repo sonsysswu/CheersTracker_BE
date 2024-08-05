fcaeb95770f70589d9f82e4b4e8581a5a7458a66
Lee <i876838@gmail.com>
2024-08-06 01:08:08 +0900

    fix : 소셜 로그인 셋팅 수정

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     nickname = models.CharField(max_length=30, unique=True)
     gender = models.CharField(max_length=10)
-    birthdate = models.DateField()
+    birthdate = models.DateField(null=True, blank=True)  # null과 blank 허용
 
     def __str__(self):
         return self.username
--- user/views.py
+++ user/views.py
@@ -1,17 +1,10 @@
-from rest_framework import generics
-from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
-from rest_framework.views import APIView
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import update_session_auth_hash
 from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ChangePasswordSerializer
+from django.contrib.auth import get_user_model, authenticate, update_session_auth_hash
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer, ChangePasswordSerializer, EncryptedUserSerializer
 from .models import CustomUser
-from .serializers import EncryptedUserSerializer
-from rest_framework.permissions import IsAdminUser
-from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
@@ -26,17 +19,15 @@ class LoginView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         
-        # 사용자가 인증되었는지 확인
         user = authenticate(username=username, password=password)
         
         if user is not None:
-            # 사용자 인증 성공 시 토큰 생성 및 반환
             token, created = Token.objects.get_or_create(user=user)
-            user_data = UserSerializer(user).data  # 사용자 데이터를 직렬화
+            user_data = UserSerializer(user).data
             return Response({'token': token.key, 'user': user_data}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
 # 비밀번호 변경 로직
 class ChangePasswordView(generics.UpdateAPIView):
     serializer_class = ChangePasswordSerializer
@@ -51,35 +42,33 @@ class ChangePasswordView(generics.UpdateAPIView):
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
-            # Check old password
             if not self.object.check_password(serializer.validated_data.get("old_password")):
                 return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            # Set new password
             self.object.set_password(serializer.validated_data.get("new_password"))
             self.object.save()
-            update_session_auth_hash(request, self.object)  # Important for keeping the user logged in
+            update_session_auth_hash(request, self.object)
             return Response({"status": "password set"}, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# 사용자 정보
-class EncryptedUserView(generics.ListAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = EncryptedUserSerializer
-    permission_classes = [permissions.IsAuthenticated]  # 인증된 사용자만 접근 허용
 
-    def get_queryset(self):
-        # 선택적으로 특정 사용자의 정보만 제공할 수 있음
-        return self.queryset.filter(username=self.request.user.username)
-    
+# 사용자 정보 조회 (현재 사용자 정보)
+class EncryptedUserView(generics.RetrieveAPIView):
+    serializer_class = EncryptedUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+# 전체 사용자 목록 조회 (관리자용)
 class UserListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]  # 관리자 권한이 있는 사용자만 접근 가능
+    permission_classes = [permissions.IsAdminUser]
 
+# 현재 사용자 상세 정보 조회
 class UserDetailView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
-        return self.request.user  # 현재 인증된 사용자 정보 반환
+        return self.request.user
