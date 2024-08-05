87d961b97eed9314710361620b1b2582a4f88a74
Lee <i876838@gmail.com>
2024-08-05 22:35:48 +0900

    fix : save method 수정

--- sobrietyRecord/models.py
+++ sobrietyRecord/models.py
@@ -14,13 +14,14 @@ class SobrietyRecord(models.Model):
     def calculate_savings_and_calories(self):
         # 평균 음주량을 기반으로 계산
         days = (self.end_date - self.start_date).days + 1
-        self.savings = days * self.average_consumption * 1000  # 예: 1잔당 1000원의 비용으로 계산
-        self.calories_saved = days * self.average_consumption * 70  # 예: 1잔당 70칼로리로 계산
-        self.save()
+        calculated_savings = days * self.average_consumption * 1000  # 예: 1잔당 1000원의 비용으로 계산
+        calculated_calories_saved = days * self.average_consumption * 70  # 예: 1잔당 70칼로리로 계산
+        return calculated_savings, calculated_calories_saved
 
     def save(self, *args, **kwargs):
-        self.calculate_savings_and_calories()
-        super().save(*args, **kwargs)
+        # 계산을 호출하고 결과를 저장하기 전에 모델 필드를 업데이트
+        self.savings, self.calories_saved = self.calculate_savings_and_calories()
+        super().save(*args, **kwargs)  # 부모 클래스의 save 메소드 호출
 
     def __str__(self):
         return f"{self.user.username} - {self.start_date} to {self.end_date}"
